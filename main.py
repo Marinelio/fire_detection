6f1b29ca0f92ba3f6281355a69b55559f1d25f27
Marinelio <marineliohidri@gmail.com>
2025-03-29 17:06:39 +0200

    multi core

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import torch
+import cv2
 from ultralytics import YOLO
 
-model=YOLO('best.pt')
-
-model.predict(source='photo.jpg',imgsz=640,conf=0.6,save=True)
+torch.set_num_threads(torch.get_num_threads())
+cv2.setNumThreads(0)  # Let OpenCV decide optimal threads
+model = YOLO('best.pt')
+model.predict(source='photo.jpg', imgsz=640, conf=0.6, save=True, device='cpu')
